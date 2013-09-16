e19690c29d7cf98fb6b3504e700833a4913a023a
JustusGS <goldstei4@grinnell.edu>
2013-09-15 21:32:56 -0500

    Better calculator

--- src/edu/grinnell/csc207/nguyenti/utils/Calculator.java
+++ src/edu/grinnell/csc207/nguyenti/utils/Calculator.java
@@ -4,40 +4,30 @@ import java.math.BigInteger;
 
 public class Calculator {
 
-    // NOTE: input must be in the form
-    // "[int] [operator] [int] [operator] ... [int]"
-    public static BigInteger eval0(String input) {
+	// NOTE: input must be in the form
+	// "[int] [operator] [int] [operator] ... [int]"
+	public static BigInteger eval0(String input) {
 
-	String[] strArray = StringUtils.splitAt(input, ' ');
-	BigInteger current = new BigInteger(strArray[0]);
+		String[] strArray = StringUtils.splitAt(input, ' ');
+		BigInteger current = new BigInteger(strArray[0]);
 
-	for (int i = 1; i < strArray.length; i += 2) {
-	    if (strArray[i].equals("+")) {
-		current = current.add(new BigInteger(strArray[i + 1]));
-	    } else {
-		if (strArray[i].equals("-")) {
-		    current = current.subtract(new BigInteger(strArray[i + 1]));
-		} else {
-		    if (strArray[i].equals("*")) {
-			current = current.multiply(new BigInteger(
-				strArray[i + 1]));
-		    } else {
-			if (strArray[i].equals("/")) {
-			    current = current.divide(new BigInteger(
-				    strArray[i + 1]));
+		for (int i = 1; i < strArray.length; i += 2) {
+			if (strArray[i].equals("+")) {
+				current = current.add(new BigInteger(strArray[i + 1]));
+			} else if (strArray[i].equals("-")) {
+				current = current.subtract(new BigInteger(strArray[i + 1]));
+			} else if (strArray[i].equals("*")) {
+				current = current.multiply(new BigInteger(strArray[i + 1]));
+			} else if (strArray[i].equals("/")) {
+				current = current.divide(new BigInteger(strArray[i + 1]));
+			} else if (strArray[i].equals("^")) {
+				current = current.pow(Integer.valueOf(strArray[i + 1]));
 			} else {
-			    if (strArray[i].equals("^")) {
-				current = current.pow(Integer
-					.valueOf(strArray[i + 1]));
-			    } else {
 				throw new IllegalArgumentException(
-					"The only legal operators are + - * / ^");
-			    }
+						"The only legal operators are + - * / ^");
 			}
-		    }
 		}
-	    }
+
+		return current;
 	}
-	return current;
-    }
 }
