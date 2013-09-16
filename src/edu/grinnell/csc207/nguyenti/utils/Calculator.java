bf4729558cefe54eb252843b62fc654f13c73405
nguyenti <tn.xc.lax@gmail.com>
2013-09-15 22:26:05 -0500

    Added more Calculator Testing

--- src/edu/grinnell/csc207/nguyenti/utils/Calculator.java
+++ src/edu/grinnell/csc207/nguyenti/utils/Calculator.java
@@ -14,30 +14,49 @@ public class Calculator {
 	for (int i = 1; i < strArray.length; i += 2) {
 	    if (strArray[i].equals("+")) {
 		current = current.add(new BigInteger(strArray[i + 1]));
-	    } else {
-		if (strArray[i].equals("-")) {
-		    current = current.subtract(new BigInteger(strArray[i + 1]));
+	    } else if (strArray[i].equals("-")) {
+		current = current.subtract(new BigInteger(strArray[i + 1]));
+	    } else if (strArray[i].equals("*")) {
+		current = current.multiply(new BigInteger(strArray[i + 1]));
+	    } else if (strArray[i].equals("/")) {
+		if (!(new BigInteger(strArray[i + 1])).equals(BigInteger
+			.valueOf(0))) {
+		    current = current.divide(new BigInteger(strArray[i + 1]));
 		} else {
-		    if (strArray[i].equals("*")) {
-			current = current.multiply(new BigInteger(
-				strArray[i + 1]));
-		    } else {
-			if (strArray[i].equals("/")) {
-			    current = current.divide(new BigInteger(
-				    strArray[i + 1]));
-			} else {
-			    if (strArray[i].equals("^")) {
-				current = current.pow(Integer
-					.valueOf(strArray[i + 1]));
-			    } else {
-				throw new IllegalArgumentException(
-					"The only legal operators are + - * / ^");
-			    }
+		    throw new ArithmeticException("Cannot divide by zero");
+		}
+	    } else if (strArray[i].equals("^")) {
+		current = current.pow(Integer.valueOf(strArray[i + 1]));
+	    } else {
+		throw new IllegalArgumentException(
+			"The only legal operators are + - * / ^");
+	    }
+	}
+	return current;
+    }
+
+    public static int[] fewestCoins(int denomination) {
+	int[] coins = { (int) Math.ceil(denomination / 2.0), 0, 0, 0 };
+	for (int twos = 0; twos < (int) Math.ceil(denomination / 2.0) + 1; twos++) {
+	    for (int sevens = 0; sevens < (int) Math.ceil(denomination / 7.0) + 1; sevens++) {
+		for (int elevens = 0; elevens < (int) Math
+			.ceil(denomination / 11.0) + 1; elevens++) {
+		    for (int fiftyfours = 0; fiftyfours < (int) Math
+			    .ceil(denomination / 54.0) + 1; fiftyfours++) {
+			if (twos * 2 + sevens * 7 + elevens * 11 + fiftyfours
+				* 54 == denomination
+				&& twos + sevens + elevens + fiftyfours < (coins[0]
+					+ coins[1] + coins[2] + coins[3])) {
+			    coins[0] = twos;
+			    coins[1] = sevens;
+			    coins[2] = elevens;
+			    coins[3] = fiftyfours;
 			}
 		    }
 		}
 	    }
 	}
-	return current;
+
+	return coins;
     }
 }
--- src/edu/grinnell/csc207/nguyenti/utils/CalculatorTest.java
+++ src/edu/grinnell/csc207/nguyenti/utils/CalculatorTest.java
@@ -1,19 +1,39 @@
 package edu.grinnell.csc207.nguyenti.utils;
 
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 import java.math.BigInteger;
-
-import org.junit.Test;
 
 public class CalculatorTest {
 
     @Test
     public void testEva10() {
-	assertEquals(BigInteger.valueOf(0), Calculator.eval0("0"));
-	assertEquals(BigInteger.valueOf(2), Calculator.eval0("1 + 1"));
-	assertEquals(BigInteger.valueOf(4), Calculator.eval0("1 + 2 + 1"));
-	assertEquals(BigInteger.valueOf(9), Calculator.eval0("1 + 2 * 3"));
+	assertEquals("0", BigInteger.valueOf(0), Calculator.eval0("0"));
+	assertEquals("1 value", BigInteger.valueOf(3), Calculator.eval0("3"));
+	assertEquals("addition", BigInteger.valueOf(2), Calculator.eval0("1 + 1"));
+	assertEquals("exponentation", BigInteger.valueOf(16), Calculator.eval0("4 ^ 2"));
+	assertEquals("division", BigInteger.valueOf(2), Calculator.eval0("6 / 3"));
+	assertEquals("subtraction", BigInteger.valueOf(2), Calculator.eval0("4 - 2"));
+	assertEquals("multiplication", BigInteger.valueOf(18), Calculator.eval0("6 * 3"));
+	assertEquals("more than 2 values addition", BigInteger.valueOf(4), Calculator.eval0("1 + 2 + 1"));
+	assertEquals("multiple operations", BigInteger.valueOf(9), Calculator.eval0("1 + 2 * 3"));
+	assertEquals("other multiple operations", BigInteger.valueOf(-1), Calculator.eval0("2 - 4 / 2"));
+    }
+
+    @Test
+    public void testEval0() {
+	int[] arrayOne = {3, 0, 1, 0};
+	int[] arrayTwo = {1, 1, 1, 1};
+	int[] arrayThree = {1, 0, 0, 0};
+	int[] arrayFour = {0, 0, 0, 0};
+	int[] arrayFive = {0, 2, 0, 0};
+	
+	assertArrayEquals("two denominations", arrayOne, Calculator.fewestCoins(17));
+	assertArrayEquals("one of each", arrayTwo, Calculator.fewestCoins(74));
+	assertArrayEquals("one denomination", arrayThree, Calculator.fewestCoins(2));
+	assertArrayEquals("0", arrayFour, Calculator.fewestCoins(0));
+	assertArrayEquals("two of a denomination", arrayFive, Calculator.fewestCoins(14));
     }
 
 }
